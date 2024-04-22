8c0935530e6c485b145751dbc678d3f12c76c699
mauro <mrb0305@gmail.com>
2024-04-21 21:00:07 -0300

    variáveis

---
+++ blocos.py
@@ -0,0 +1,6 @@
+idade = 17
+
+if idade < 18:
+    print("menor de idade")
+else:
+    print("maior de idade")
---
+++ variaveis.py
@@ -0,0 +1,6 @@
+
+nome_completo = 'Mauro Rodrigues Braga'
+idade = 25
+
+
+print(nome_completo)
