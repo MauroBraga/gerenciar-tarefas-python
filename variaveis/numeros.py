1c7c03f67b3a94cfee7e1e695758cf593381f825
mauro <mrb0305@gmail.com>
2024-04-22 10:36:37 -0300

    numeoros

---
+++ variaveis/numeros.py
@@ -0,0 +1,14 @@
+#Inteiro
+numero_inteiro =3
+
+#Real com ponto flutuante
+numero_real = 3.14
+
+print("Inteiro=",numero_inteiro)
+
+print("Realcom ponto flutuante=",numero_real)
+
+
+print("Tipo da variavel inteiro=", type(numero_inteiro))
+
+print("Tipo da variavel real=", type(numero_real))
