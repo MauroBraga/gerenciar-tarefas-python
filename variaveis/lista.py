e2fdb1135764bc9b6513bac097ef76089b19be2b
mauro <mrb0305@gmail.com>
2024-04-23 10:24:25 -0300

    Lista

---
+++ variaveis/lista.py
@@ -0,0 +1,14 @@
+#Declaração
+
+minha_lista = [1,2,3,4,5, "rocktseat", True, False]
+
+print("Minha lista de exemplo=", minha_lista)
+
+print("minha_lista[0]=",minha_lista[0])
+print("minha_lista[5]=",minha_lista[5])
+
+print("minha_lista[1:7]=",minha_lista[1:7])
+
+print("minha_lista[:6]=",minha_lista[:6])
+
+print("minha_lista[2:]=",minha_lista[2:])
