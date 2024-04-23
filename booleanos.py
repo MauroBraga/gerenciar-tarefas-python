3c787a99235a3f690961e6fd8d7b1d95ed17d78b
mauro <mrb0305@gmail.com>
2024-04-22 22:06:14 -0300

    Booleanos

---
+++ booleanos.py
@@ -0,0 +1,34 @@
+#Condição Verdadeira
+True
+
+#Condição FAlsa
+False
+
+
+#Condicionais
+if True:
+    print("Bloco IF")
+else:
+    print("Bloco False")
+
+# Operadores lógicos: and e or
+
+# AND
+if True and True:
+    print("Bloco será executado")
+
+if True and False:
+    print("Bloco não será executado")
+
+if False and False:
+    print("Bloco não será executado")
+
+# OR
+if True or False:
+    print("Bloco OR vai ser executado")
+
+if False or False:
+    print("Bloco OR não vai ser executado")
+
+if True or True:
+    print("Bloco OR vai ser executado")
